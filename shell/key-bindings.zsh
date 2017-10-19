a56d0d8777dd53e67224cf0a0ed8db4bae1cf76c
dsh2 <dsh2@me.com>
2017-10-19 21:54:38 +0200

    fzf: Further tweak __fsel

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -3,24 +3,30 @@
 if [[ $- == *i* ]]; then
 
 # CTRL-T - Paste the selected file path(s) into the command line
+# TODO: Use a more advanced preview than strings
 __fsel() {
     setopt localoptions pipefail 
     REPORTTIME=-1
+    FIND_PRINTF='%y%m\t%n\t%TY-%Tm-%Td\t%TH:%TM\t%u:%g\t%kk\t%p\n'
+    FIND_COLUMNS=${(ws:\t:)#FIND_PRINTF}
     command find \
 		-P . \
 		-mindepth 1  \
-		-printf '%y%m\t%n\t%TY-%Tm-%Td\t%TH:%TM\t%u:%g\t%kk\t%p\n' |
+		-printf $FIND_PRINTF |
 	 $(__fzfcmd) \
+	     --bind "ctrl-r:execute(tmux split -h ranger {$FIND_COLUMNS..})" \
+	     --bind "ctrl-v:execute(tmux split -h vim {$FIND_COLUMNS..})" \
 	     --sort \
 	     --multi \
+	     --tabstop=6 \
 	     --preview-window=top:50% \
 	     --preview="
-		    echo -n \"file: \"; 
+		    echo -n \"file: \";
 		    file --brief \
 			    --preserve-date \
 			    --special-files \
-			    --uncompress {7..}; \
-		    strings -f {7..}" | 
+			    --uncompress {$FIND_COLUMNS..}; \
+		    strings {$FIND_COLUMNS..}" |
     while read item; do
 	local file=$(echo $item | cut -f 7-)
 	echo -n "${(q)file} "
@@ -63,12 +69,14 @@ bindkey '\ec' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
-  local selected num
-  setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected=( $(fc -rlEDt '%a %F  %T' 1 |
+    setopt localoptions noglobsubst noposixbuiltins pipefail 
+    local selected num
+    selected=( $(fc -rlEDt '%a %F  %T' 1 |
 		$(__fzfcmd) \
 		--no-sort \
-		--preview 'echo {6..} | pygmentize -l zsh; tmux-log.sh {1}' \
+		--preview "
+		    echo {6..} | pygmentize -l zsh;
+		    tmux-log.sh {1}" \
 		--preview-window up:5:wrap \
 		--tiebreak=begin,index  \
 		--toggle-sort=ctrl-r  \
@@ -76,15 +84,14 @@ fzf-history-widget() {
 		-q "${LBUFFER//$/\\$}"
 		)
 	    )
-  # selected=( $(fc -liED 1 | $(__fzfcmd) +s --tac +m -n4.. --tiebreak=index --toggle-sort=ctrl-r ${=FZF_CTRL_R_OPTS} -q "${LBUFFER//$/\\$}") )
-  local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
+    local ret=$?
+    if [ -n "$selected" ]; then
+	num=$selected[1]
+	if [ -n "$num" ]; then
+	    zle vi-fetch-history -n $num
+	fi
     fi
-  fi
-  zle redisplay
+    zle redisplay
 }
 zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
