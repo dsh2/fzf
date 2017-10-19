9fdbc86d8427d13a06ca2bd3e2c0dd5a068cea4c
dsh2 <dsh2@me.com>
2017-10-19 02:46:40 +0200

    tweak fset to support ranger, tweak history to use fc -r instead of --tac

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -5,14 +5,14 @@ if [[ $- == *i* ]]; then
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
     # local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -P . -mindepth 1  \
-    -printf '%y%m %n %TY-%Tm-%Td %TH:%TM %u:%g %kk %p\n' 2> /dev/null" }"
-  setopt localoptions pipefail 2> /dev/null
-  # eval "$cmd" | $(__fzfcmd) +s --preview="file {}; ~/.config/ranger/scope.sh {}" -m | while read item; do
-  eval "$cmd" | $(__fzfcmd) +s --tac -m --preview-window=top:50% --preview="echo -n \"file: \"; file --brief --preserve-date --special-files --uncompress {7..}; strings {7..}" -m | while read item; do
-      local file=$(echo $item | cut -d ' ' -f 7-)
-      echo -n "${(q)file} "
-  done
+	  local cmd="${FZF_CTRL_T_COMMAND:-"command find -P . -mindepth 1  \
+			-printf '%y%m %n %TY-%Tm-%Td %TH:%TM %u:%g %kk %p\n' 2> /dev/null" }"
+	  setopt localoptions pipefail 2> /dev/null
+	  # eval "$cmd" | $(__fzfcmd) +s --preview="file {}; ~/.config/ranger/scope.sh {}" -m | while read item; do
+	  eval "$cmd" | $(__fzfcmd) +s --tac -m --preview-window=top:50% --preview="echo -n \"file: \"; file --brief --preserve-date --special-files --uncompress {7..}; strings {7..}" -m | while read item; do
+		  local file=$(echo $item | cut -d ' ' -f 7-)
+		  echo -n "${(q)file} "
+	  done
   echo
 }
 
@@ -34,7 +34,7 @@ bindkey '^T' fzf-file-widget
 
 # ALT-C - cd into the selected directory
 fzf-cd-widget() {
-  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 -maxdepth 5 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type d -print 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail 2> /dev/null
   local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
@@ -53,7 +53,17 @@ bindkey '\ec' fzf-cd-widget
 fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
-  selected=( $(fc -lEDt '%a %F %T' 1 | $(__fzfcmd) +s --tac +m -n4..,.. --preview 'echo {} | pygmentize -l zsh' --preview-window up:3:wrap --tiebreak=index --toggle-sort=ctrl-r ${=FZF_CTRL_R_OPTS} -q "${LBUFFER//$/\\$}") )
+  selected=( $(fc -rlEDt '%a %F  %T' 1 |
+		$(__fzfcmd) \
+		--no-sort \
+		--preview 'echo {6..} | pygmentize -l zsh; tmux-log.sh {1}' \
+		--preview-window up:5:wrap \
+		--tiebreak=begin,index  \
+		--toggle-sort=ctrl-r  \
+		${=FZF_CTRL_R_OPTS} \
+		-q "${LBUFFER//$/\\$}"
+		)
+	    )
   # selected=( $(fc -liED 1 | $(__fzfcmd) +s --tac +m -n4.. --tiebreak=index --toggle-sort=ctrl-r ${=FZF_CTRL_R_OPTS} -q "${LBUFFER//$/\\$}") )
   local ret=$?
   if [ -n "$selected" ]; then
